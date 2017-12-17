d8961ab1c0729a529878fec2d06a2960344f944d
Chris Watkins (macbook) <christopher.watkins@me.com>
2017-12-18 10:52:17 +1100

    More cross compatibility changes

--- logslacker/tests/test_logslacker.py
+++ logslacker/tests/test_logslacker.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from six import string_types
 
 import logslacker
 
@@ -6,4 +7,4 @@ class TestSlacker(TestCase):
     def test_is_string(self):
         h = logslacker.SlackLogHandler()
         s = h.test()
-        self.assertTrue(isinstance(s, basestring))
+        self.assertTrue(isinstance(s, string_types))
